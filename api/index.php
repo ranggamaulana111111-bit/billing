141565f1f8e2e766c94e043b1717b5ad1e25dbdb
ranggadydriver-cmyk <ranggadydriver@gmail.com>
2026-06-12 03:32:38 +0700

    fix: single-bootstrap api/index + auto migrate

--- api/index.php
+++ api/index.php
@@ -1,11 +1,15 @@
 <?php
 
-$app = require __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->call('migrate', ['--force' => true]);
-
 $_SERVER['SCRIPT_NAME'] = '/index.php';
 $_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
 
-require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require __DIR__ . '/../bootstrap/app.php';
+
+$app->make(Illuminate\Contracts\Console\Kernel::class)->call('migrate', ['--force' => true]);
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle($request = Illuminate\Http\Request::capture());
+$response->send();
+$kernel->terminate($request, $response);
