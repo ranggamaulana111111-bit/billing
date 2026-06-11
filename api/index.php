ccd5da697062983d28e4101a9476ce01f197a4ef
ranggadydriver-cmyk <ranggadydriver@gmail.com>
2026-06-12 03:28:58 +0700

    fix: auto-migrate on cold start

--- api/index.php
+++ api/index.php
@@ -1,5 +1,10 @@
 <?php
 
+$app = require __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel->call('migrate', ['--force' => true]);
+
 $_SERVER['SCRIPT_NAME'] = '/index.php';
 $_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
 
